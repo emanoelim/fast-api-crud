abc0a91c59c5ee77b9574e74c544d74eb5990954
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2022-02-15 22:49:23 -0300

    list notes

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -23,3 +23,45 @@ def test_create_note(test_app, monkeypatch):
 def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
     assert response.status_code == 422
+
+
+def test_read_note(test_app, monkeypatch):
+    test_data = {"id": 1, "title": "something", "description": "something else"}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/notes/1")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_read_note_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/notes/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Note not found"
+
+
+def test_read_all_notes(test_app, monkeypatch):
+    test_data = [
+        {"title": "something", "description": "something else", "id": 1},
+        {"title": "someone", "description": "someone else", "id": 2},
+    ]
+
+    async def mock_get_all():
+        return test_data
+
+    monkeypatch.setattr(crud, "get_all", mock_get_all)
+
+    response = test_app.get("/notes/")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
