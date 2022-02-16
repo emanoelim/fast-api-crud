1c6ac94ac171a84cb1755ac0744bf508d15e622a
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2022-02-16 08:03:13 -0300

    update note

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -65,3 +65,38 @@ def test_read_all_notes(test_app, monkeypatch):
     assert response.json() == test_data
 
 
+def test_update_note(test_app, monkeypatch):
+    test_update_data = {"title": "someone", "description": "someone else", "id": 1}
+
+    async def mock_get(id):
+        return True
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    async def mock_put(id, payload):
+        return 1
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    response = test_app.put("/notes/1/", data=json.dumps(test_update_data))
+    assert response.status_code == 200
+    assert response.json() == test_update_data
+
+
+@pytest.mark.parametrize(
+    "id, payload, status_code",
+    [
+        [1, {}, 422],
+        [1, {"description": "bar"}, 422],
+        [999, {"title": "foo", "description": "bar"}, 404],
+    ],
+)
+def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.put(f"/notes/{id}/", data=json.dumps(payload),)
+    assert response.status_code == status_code
+
